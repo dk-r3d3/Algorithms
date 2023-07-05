3a6fe41163df06f5a150156fef10738b2df32100
dk_r3 <dk.r3d3@gmail.com>
2023-07-05 21:28:45 +0700

    initial

---
+++ sorted/C#/buble/Program.cs
@@ -0,0 +1,19 @@
+﻿// сортировка пузырьком на C#
+
+void BubleSorted(int[] collection)
+{
+    for (int i = 1; i < collection.Length; i++)
+    {
+        for (int j =0; j < collection.Length; j++)
+        {
+            if(collection[i] < collection[j])
+            {
+                (collection[i], collection[j]) = (collection[j], collection[i]);
+            }
+        }
+    }
+}
+
+int[] array = new int[]{3, 1, 4, 8, 5, 0, 3};
+BubleSorted(array);
+System.Console.WriteLine(string.Join(", ", array));
